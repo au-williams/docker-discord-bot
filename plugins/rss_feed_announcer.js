fd4098a6a497e485ff0e7cb8a845c5b28be75ded
Austin Williams <me@austinwilliams.dev>
2025-03-22 00:41:21 -0700

    improved rss plugins json config integration

--- plugins/rss_feed_announcer.js
+++ plugins/rss_feed_announcer.js
@@ -1,4 +1,4 @@
-import { AttachmentBuilder, EmbedBuilder, Events } from "discord.js";
+import { AttachmentBuilder, EmbedBuilder } from "discord.js";
 import { Config } from "../services/config.js";
 import { getLinkPreview } from "link-preview-js";
 import { Logger } from "../services/logger.js";
@@ -7,13 +7,13 @@ import { Utilities } from "../services/utilities.js";
 import * as URL from "url";
 import CronJob from "../entities/CronJob.js";
 import date from "date-and-time";
-import Listener from "../entities/Listener.js";
 import ordinal from "date-and-time/plugin/ordinal";
 import Parser from "rss-parser";
 date.plugin(ordinal);
 
 const config = new Config(import.meta.filename);
 const logger = new Logger(import.meta.filename);
+
 const parser = new Parser();
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -30,12 +30,6 @@ export const CronJobs = new Set([
     .setTriggered()
 ]);
 
-export const Listeners = Object.freeze({
-  [Events.MessageDelete]: new Listener()
-    .setFunction(Utilities.deleteMessageThread)
-    .setRequiredChannels(config.announcement_discord_channel_ids)
-});
-
 ///////////////////////////////////////////////////////////////////////////////
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
 // #endregion EMITTER.JS IMPORTS                                             //
@@ -49,38 +43,39 @@ export const Listeners = Object.freeze({
 ///////////////////////////////////////////////////////////////////////////////
 
 /**
- * TODO: Move this to utils, use in steam plugin
- */
-export function removeTags(str) {
-  return str
-    .replace(/[\u2018\u2019\u201A\u201B\u2032\u2035']/g, "'")
-    .replace(/<h[1-6]>(.*?)<\/h[1-6]>/gi, "$1:")
-    .replace(/(<([^>]+)>)/ig, "")
-    .replace(/\s+/g, " ");
-}
-
-/**
  * Check for pending announcements on startup and a regular time interval
  * @param {object} param
  * @param {Client} param.client The Discord.js client
  * @param {Listener} param.listener
  */
 export async function checkAndAnnounceRssUpdate({ client, listener }) {
-  for(const { url, ignored_strings, required_strings } of config.rss_feeds) {
-    const rss = await parser.parseURL(url).then(({ items }) =>
-      items.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate)).find(item => {
-        const isIgnoredMet = !ignored_strings?.length || !ignored_strings.some(str => item.title.toLowerCase().includes(str.toLowerCase()));
-        const isRequirementMet = !required_strings?.length || required_strings.some(str => item.title.toLowerCase().includes(str.toLowerCase()));
-        return isIgnoredMet && isRequirementMet;
+  for(const rss_feed of config.announcement_rss_feeds) {
+    const {
+      discord_announcement_channel_ids, discord_embed_image,
+      discord_embed_thumbnail, discord_embed_title, rss_feed_url,
+      rss_content_ignored_strings, rss_content_required_strings,
+      rss_title_ignored_strings, rss_title_required_strings
+    } = rss_feed;
+
+    const rss = await parser.parseURL(rss_feed_url).then(({ items }) =>
+      items.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate)).find(({ content, title }) => {
+        const includes = (source, str) => source.toLowerCase().includes(str.toLowerCase());
+        const isContentIgnoredMet = !rss_content_ignored_strings?.length || !rss_content_ignored_strings.some(str => includes(content, str));
+        const isContentRequirementMet = !rss_content_required_strings?.length || rss_content_required_strings.some(str => includes(content, str));
+        const isTitleIgnoredMet = !rss_title_ignored_strings?.length || !rss_title_ignored_strings.some(str => includes(title, str));
+        const isTitleRequirementMet = !rss_title_required_strings?.length || rss_title_required_strings.some(str => includes(title, str));
+        return isContentIgnoredMet && isContentRequirementMet && isTitleIgnoredMet && isTitleRequirementMet;
       })
     );
 
-    for(const channel_id of config.announcement_discord_channel_ids) {
+    if (!rss) continue; // No suitable article was found!
+
+    for(const channel_id of discord_announcement_channel_ids) {
       const existingMessage = Messages
         .get({ channelId: channel_id })
         .find(message => message.embeds?.[0]?.data.description?.includes(rss.link));
 
-      if (existingMessage) continue;
+      if (existingMessage) continue; // Article was already sent!
 
       const hostName = URL.parse(rss.link).hostname;
       const articlePreview = await getLinkPreview(rss.link);
@@ -89,16 +84,18 @@ export async function checkAndAnnounceRssUpdate({ client, listener }) {
       const content = rss["content:encoded"] || rss.content;
       const description = Utilities.getTruncatedStringTerminatedByWord(removeTags(content), 133);
       const formattedDate = date.format(new Date(rss.pubDate), "MMMM DDD");
-      const siteName = websitePreview.siteName || websitePreview.title;
+      const title = discord_embed_title || websitePreview.siteName || websitePreview.title;
+
+      // TODO: check if discord_embed_image and discord_embed_thumbnail include "https://" else upload file
 
       const embeds = [new EmbedBuilder()
         .setAuthor({ name: "New RSS announcement", iconURL: "attachment://rss_logo.png" })
         .setColor(0xF26109)
         .setDescription(`- [**${rss.title}**](${rss.link})\n${`_${description}_`}`)
         .setFooter({ text: `Posted on ${formattedDate}. Click the link to read the full announcement.` })
-        .setImage(articlePreview.images[0])
-        .setThumbnail(websitePreview.images[0])
-        .setTitle(siteName)];
+        .setImage(discord_embed_image || articlePreview.images[0])
+        .setThumbnail(discord_embed_thumbnail || websitePreview.images[0])
+        .setTitle(title)];
 
         const files = [new AttachmentBuilder("assets/rss_logo.png")];
         const channel = client.channels.cache.get(channel_id);
@@ -106,7 +103,7 @@ export async function checkAndAnnounceRssUpdate({ client, listener }) {
         Utilities.LogPresets.SentMessage(message, listener);
 
         const name =
-          Utilities.getTruncatedStringTerminatedByChar(`💬 ${siteName} - ${rss.title}`, 100); // maximum thread name size
+          Utilities.getTruncatedStringTerminatedByChar(`💬 ${title} - ${rss.title}`, 100); // maximum thread name size
 
         message
           .startThread({ name })
@@ -114,6 +111,17 @@ export async function checkAndAnnounceRssUpdate({ client, listener }) {
           .catch(error => logger.error(error, listener));
     }
   }
+}
+
+/**
+ * TODO: Move this to utils, use in steam plugin
+ */
+export function removeTags(str) {
+  return str
+    .replace(/[\u2018\u2019\u201A\u201B\u2032\u2035']/g, "'")
+    .replace(/<h[1-6]>(.*?)<\/h[1-6]>/gi, "$1:")
+    .replace(/(<([^>]+)>)/ig, "")
+    .replace(/\s+/g, " ");
 }
 
 ///////////////////////////////////////////////////////////////////////////////
