b8fc4bd5a77c6558e72ca355f4993113a7849f2c
Austin Williams <me@austinwilliams.dev>
2025-03-29 03:12:43 -0700

    added playlist support to plex media server plugin

--- plugins/plex_media_server_announcer.js
+++ plugins/plex_media_server_announcer.js
@@ -115,129 +115,139 @@ export async function checkAndUpdateActivity({ client, listener }) {
  * @param {Listener} param.listener
  */
 export async function checkAndAnnounceUpdates({ client, listener }) {
-  const plexRecentItems = await fetchPlexRecentItems();
+  // Sort items by aired order and group by parent id
+  console.log(await fetchPlexPlaylistItems(config.plex_playlist_keys))
+  const plexRecentItems = await fetchPlexPlaylistItems(config.plex_playlist_keys)
+    .then(items => items.sort((a, b) => a.parentIndex - b.parentIndex || a.index - b.index).reduce((previous, current) => {
+      const highestRatingKey = current.grandparentRatingKey || current.parentRatingKey || current.ratingKey;
+      previous[highestRatingKey] ??= { episodes: [], ...current };
+      if (current.type !== "episode") return previous;
+      previous[highestRatingKey].episodes.push(current);
+      return previous;
+    }, {}));
+
   let plexServer;
 
-  for(const plexRecentItem of plexRecentItems) {
-    const isMovie = plexRecentItem.librarySectionTitle.toLowerCase().includes("movie");
-    const isShow = plexRecentItem.librarySectionTitle.toLowerCase().includes("show");
-
-    ///////////////////////////////////////////////////////////////////////////
-    // Assign as needed so we don't make a do of unnecessary fetch requests! //
-    ///////////////////////////////////////////////////////////////////////////
+  // Loop each item, with each item possibly containing a list
+  for(const highestRatingKey of Object.keys(plexRecentItems)) {
+    const recentItem = plexRecentItems[highestRatingKey];
+    const metadata = await fetchPlexMetadata(highestRatingKey);
 
     let channel;
-    let embeds;
-    let embedAuthor;
-    let embedDescription;
-    let embedFields;
-    let embedFooter;
+
+    let embedTitle = `${metadata.title} (${metadata.year})`;
+
     let embedImage;
+    let embeds;
     let embedThumbnail;
-    let shortDate;
     let files;
     let imdbGuid;
+    let shortDate;
 
-    // Pre-format the embed title so we can check its existence in the channel.
-    let embedTitle =
-      `${plexRecentItem.parentTitle || plexRecentItem.title} (${plexRecentItem.parentYear || plexRecentItem.year})`;
-
-    if (isShow && plexRecentItem.type === "season") {
-      embedTitle += ` \`Season ${plexRecentItem.index}\``;
-    }
-
-    if (isShow && plexRecentItem.type !== "season") {
-      // TODO: I've only seen "season" so I don't know if this exists. But it
-      // has a field and that implies it changes so does "episode" exist too?
-      embedTitle += ` \`${plexRecentItem.title}\``;
-    }
+    // Everything added BEFORE last message was sent will not be sent.
 
     for(const channel_id of config.discord_announcement_channel_ids) {
       const existingMessage = Messages
         .get({ channelId: channel_id })
         .find(({ embeds }) => embeds?.[0]?.data.title === embedTitle);
 
-      if (existingMessage) continue; // Message was already sent!
+      let embedAuthor;
+
+      if (recentItem.type === "movie") {
+        if (existingMessage) continue;
+        embedAuthor = "New movie in Plex Media Server";
+      }
+
+      let unannouncedEpisodes;
+
+      if (recentItem.type === "episode") {
+        if (existingMessage) {
+          const filterDate = item => new Date(item.addedAt * 1000) > existingMessage.createdAt
+          unannouncedEpisodes = recentItem.episodes.filter(filterDate);
+          if (!unannouncedEpisodes.length) continue; // Message was already sent!
+        }
+        else {
+          unannouncedEpisodes = recentItem.episodes;
+        }
+
+        embedAuthor= `New ${Utilities.getPluralizedString("episode", unannouncedEpisodes)} in Plex Media Server`;
+      }
 
       plexServer ??= await fetchPlexServer();
 
       if (!imdbGuid) {
-        const guids = await fetchPlexGuids(plexRecentItem);
-        imdbGuid = guids.find(id => id.startsWith("imdb://")).replace("imdb://", "");
+        // const guids = await fetchPlexGuids(highestRatingKey);
+        imdbGuid = metadata.Guid?.map(({ id }) => id).find(id => id.startsWith("imdb://"))?.replace("imdb://", "");
       }
 
-      if (!embedAuthor && isMovie) {
-        embedAuthor = "New movie in Plex Media Server";
+      const embedFields = [];
+      const value1 = `[IMDb (${imdbGuid})](https://www.imdb.com/title/${imdbGuid}/)`;
+      const value2 = `[Plex (${recentItem.librarySectionTitle})](https://app.plex.tv/desktop#!/server/${plexServer.machineIdentifier}/details?key=/library/metadata/${highestRatingKey})`;
+      embedFields.push({ inline: true, name: "View Details", value: value1 }, { inline: true, name: "View Details", value: value2 });
+
+      if (unannouncedEpisodes?.length === 3) {
+        const listItems = unannouncedEpisodes.slice(0, 3);
+        let value0 = listItems.map(i => `- \`${formatSeasonEpisodeLabel(i)}\` ["${i.title}"](https://app.plex.tv/desktop#!/server/${plexServer.machineIdentifier}/details?key=/library/metadata/${recentItem.ratingKey})`).join("\n");
+        embedFields.unshift({ name: "Episodes Added", value: value0 });
       }
 
-      if (!embedAuthor && isShow) {
-        // TODO: I don't know if "episode" exists in the Plex response. I have not seen it.
-        embedAuthor = `New ${plexRecentItem.type === "season" ? "season" : "episode"} in Plex Media Server`;
+      else if (unannouncedEpisodes?.length < 3 || unannouncedEpisodes?.length > 3) {
+        const listItems = unannouncedEpisodes.slice(0, 2);
+        const remainingCount = unannouncedEpisodes.length - listItems.length;
+        // let value0 = listItems.map(i => `- \`${formatSeasonEpisodeLabel(i)}\` "${i.title}"`).join("\n");
+        let value0 = listItems.map(i => `- \`${formatSeasonEpisodeLabel(i)}\` ["${i.title}"](https://app.plex.tv/desktop#!/server/${plexServer.machineIdentifier}/details?key=/library/metadata/${recentItem.ratingKey})`).join("\n");
+        if (remainingCount) value0 += `\n- ... and ${remainingCount} more`;
+        embedFields.unshift({ name: `${Utilities.getPluralizedString("Episode", unannouncedEpisodes)} Added`, value: value0 });
       }
-
-      embedDescription ??=
-        `_${escapeNumberedList(Utilities.getTruncatedStringTerminatedByWord(plexRecentItem.parentSummary || plexRecentItem.summary, 200))}_`;
-
-      embedFields ??= [
-        {
-          inline: true,
-          name: "View Details",
-          value: `[IMDb (${imdbGuid})](https://www.imdb.com/title/${imdbGuid}/)`
-        },
-        {
-          inline: true,
-          name: "View Details",
-          value: `[Plex (${plexRecentItem.librarySectionTitle})](https://app.plex.tv/desktop#!/server/${plexServer.machineIdentifier}/details?key=/library/metadata/${plexRecentItem.parentRatingKey || plexRecentItem.ratingKey})`
-        }
-      ];
 
       // Fetch the file from the relative Plex link. Using the URL as-is does not load in Discord.
       embedImage ??= !config.discord_enable_compact_embed && await new Downloader({
         directory: `${config.temp_directory_path}/${nanoid()}`,
         fileName: "embed_image.jpg",
-        url: `${config.plex_server_url}${plexRecentItem.art}.jpg`
+        url: `${config.plex_server_url}${recentItem.art}.jpg`
       }).download().then(({ filePath }) => filePath);
 
       // Fetch the file from the relative Plex link. Using the URL as-is does not load in Discord.
       embedThumbnail ??= await new Downloader({
         directory: `${config.temp_directory_path}/${nanoid()}`,
         fileName: "embed_thumbnail.jpg",
-        url: `${config.plex_server_url}${plexRecentItem.parentThumb || plexRecentItem.thumb}.jpg`
+        // url: `${config.plex_server_url}${(metadata.Image.find(i => i.type === "clearLogo") || metadata.Image.find(i => i.type === "coverPoster")).url}.jpg`
+        url: `${config.plex_server_url}${recentItem.grandparentThumb || recentItem.parentThumb || recentItem.thumb}.jpg`
       }).download().then(({ filePath }) => filePath);
 
-      files ??= [new AttachmentBuilder("assets/plex_logo.webp"), new AttachmentBuilder(embedThumbnail)];
+      files ??= [
+        new AttachmentBuilder("assets/plex_logo.webp"),
+        new AttachmentBuilder(embedImage),
+        new AttachmentBuilder(embedThumbnail)
+      ];
+
+      shortDate ??=
+        date.format(new Date(1000 * recentItem.addedAt), "MMMM DDD");
 
       embeds ??= [new EmbedBuilder()
         .setAuthor({ name: embedAuthor, iconURL: "attachment://plex_logo.webp" })
         .setColor(0xFF9E16)
-        .setDescription(embedDescription)
+        .setDescription(`_${escapeNumberedList(Utilities.getTruncatedStringTerminatedByWord(metadata.summary, 200))}_`)
         .addFields(...embedFields)
+        .setFooter({ text: `Added on ${shortDate} using PMS version ${plexServer.version}.` })
+        .setImage(`attachment://${path.basename(embedImage)}`)
         .setThumbnail(`attachment://${path.basename(embedThumbnail)}`)
         .setTitle(`${embedTitle} `)];
-
-      if (!config.discord_enable_compact_embed) {
-        files.push(new AttachmentBuilder(embedImage));
-        shortDate ??= date.format(new Date(1000 * (plexRecentItem.updatedAt || plexRecentItem.addedAt)), "MMMM DDD");
-        embedFooter ??= `${plexRecentItem.updatedAt ? "Updated" : "Added"} on ${shortDate} using PMS version ${plexServer.version}.`;
-        embeds[0].setFooter({ text: embedFooter });
-        embeds[0].setImage(`attachment://${path.basename(embedImage)}`);
-      }
 
       channel ??= client.channels.cache.get(channel_id);
       const message = await channel.send({ embeds, files });
       Utilities.LogPresets.SentMessage(message, listener);
 
-      const threadOptions = { name: `💬 Plex Media Server • ${plexRecentItem.parentTitle || plexRecentItem.title} (${plexRecentItem.parentYear || plexRecentItem.year})` };
+      const threadOptions = { name: `💬 Plex Media Server • ${embedTitle}` };
       const threadChannel = await Utilities.getOrCreateThreadChannel({ message, threadOptions });
 
-      const ratingKey = plexRecentItem.parentRatingKey || plexRecentItem.ratingKey;
-      const replyButton1 = buttonSubscribeMe.setCustomId(`${Interactions.ButtonSubscribeMe}${JSON.stringify({ ratingKey })}`);
-      const replyButton2 = buttonUnsubscribeMe.setCustomId(`${Interactions.ButtonUnsubscribeMe}${JSON.stringify({ ratingKey })}`);
+      const replyButton1 = buttonSubscribeMe.setCustomId(`${Interactions.ButtonSubscribeMe}${JSON.stringify({ ratingKey: highestRatingKey })}`);
+      const replyButton2 = buttonUnsubscribeMe.setCustomId(`${Interactions.ButtonUnsubscribeMe}${JSON.stringify({ ratingKey: highestRatingKey })}`);
       const replyComponents = [new ActionRowBuilder().addComponents(replyButton1, replyButton2, Emitter.moreInfoButton)];
 
-      const subscribers = config.discord_subscribed_users[ratingKey]?.filter(userId => userId.trim()).map(userId => `<@${userId}>`);
+      const subscribers = config.discord_subscribed_users[highestRatingKey]?.filter(userId => userId.trim()).map(userId => `<@${userId}>`);
       const replyContent = subscribers?.length && `📨 ${subscribers.join(" ")}`;
-      const replyDescription = `Press the \`🟩🔔 Subscribe me\` button to be alerted when new ${plexRecentItem.parentTitle || plexRecentItem.title} (${plexRecentItem.parentYear || plexRecentItem.year}) announcements are sent to ${channel} 📬`;
+      const replyDescription = `Press the \`🟩🔔 Subscribe me\` button to be alerted when new ${embedTitle} announcements are sent to ${channel} 📬`;
       const replyEmbeds = [new EmbedBuilder().setDescription(replyDescription)];
 
       const replyOptions = { components: replyComponents, embeds: replyEmbeds };
@@ -249,6 +259,34 @@ export async function checkAndAnnounceUpdates({ client, listener }) {
         .catch(error => logger.error(error, listener));
     }
   }
+}
+
+export async function fetchPlexMetadata(key) {
+  return await fetch(`${config.plex_server_url}/library/metadata/${key}`, {
+    headers: { "Accept": "application/json", "X-Plex-Token": config.plex_access_token },
+    method: "GET"
+  })
+  .then(response => response.json())
+  .then(response => response.MediaContainer?.Metadata?.[0]);
+}
+
+/**
+ *
+ */
+export async function fetchPlexPlaylistItems(playlistKeys) {
+  const result = [];
+
+  for (const playlistKey of playlistKeys) {
+    await fetch(`${config.plex_server_url}/playlists/${playlistKey}/items`, {
+      headers: { "Accept": "application/json", "X-Plex-Token": config.plex_access_token },
+      method: "GET"
+    })
+    .then(response => response.json())
+    .then(response => result.push(response.MediaContainer?.Metadata));
+  }
+
+  // TODO: this should be cleaned up
+  return result.flat().filter(Boolean);
 }
 
 /**
@@ -272,40 +310,16 @@ export async function fetchPlexServer() {
     method: "GET"
   })
   .then(response => response.json())
-  .then(response => response.MediaContainer.Server[0]);
+  .then(response => response.MediaContainer?.Server?.[0]);
 }
 
 /**
  *
  */
-export async function fetchPlexGuids(plexRecentItem) {
-  return await fetch(`${config.plex_server_url}/library/metadata/${plexRecentItem.parentRatingKey || plexRecentItem.ratingKey}`, {
-    headers: {
-      "Accept": "application/json",
-      "X-Plex-Token": config.plex_access_token
-    },
-    method: "GET"
-  })
-  .then(response => response.json())
-  .then(response => response.MediaContainer.Metadata[0].Guid.map(({ id }) => id));
-}
-
-/**
- *
- */
-export async function fetchPlexRecentItems() {
-  return await fetch(`${config.plex_server_url}/library/recentlyAdded`, {
-      headers: {
-        "Accept": "application/json",
-        "X-Plex-Token": config.plex_access_token
-      },
-      method: "GET"
-    })
-    .then(response => response.json())
-    .then(response => response.MediaContainer.Metadata.filter(item =>
-      item.librarySectionTitle.toLowerCase().includes("movies")
-      || item.librarySectionTitle.toLowerCase().includes("shows")
-    ).slice(0, config.plex_recent_item_count));
+export function formatSeasonEpisodeLabel(item) {
+  const s = `S${item.parentIndex < 10 ? "0" : ""}${item.parentIndex}`;
+  const e = `E${item.index < 10 ? "0" : ""}${item.index}`;
+  return `${s}${e}`;
 }
 
 /**
